d8af22978b72cf85a454f2c796fb3d8de87fe8f9
RaccoonRyu <rjhwind94@gmail.com>
2022-04-07 15:22:50 +0900

    초안반영(Spring web, devtools, lombok, JPA) 커밋

---
+++ src/main/java/org/zerock/exboot1/Exboot1Application.java
@@ -0,0 +1,13 @@
+package org.zerock.exboot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exboot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exboot1Application.class, args);
+	}
+
+}
